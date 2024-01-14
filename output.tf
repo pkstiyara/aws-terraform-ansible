daf5fb5489caf7d2970b48283c5268b2f7647815
Pankaj Singh <pkstiyara@gmail.com>
2024-01-14 09:16:00 +0000

    uipdated output

--- output.tf
+++ output.tf
@@ -1,24 +1,26 @@
 
-output "master-node-ip" {
-    value = aws_instance.master-node.public_ip  
+output "node-1-ip" {
+    value = aws_instance.node-1.public_ip 
 }
 
-output "worker-node-1-ip" {
-    value = aws_instance.worker-node-1.public_ip
+output "node-1-ssh" {
+    value = "ssh -i my_key_pair ec2-user@${aws_instance.node-1.public_ip}"
 }
 
-output "worker-node-2-ip" {
-    value = aws_instance.worker-node-2.public_ip  
+
+output "node-2-ip" {
+    value = aws_instance.node-2.public_ip 
 }
 
-output "master-node-ssh" {
-    value = "ssh -i my_key_pair ec2-user@${aws_instance.master-node.public_ip}"
+output "node-2-ssh" {
+    value = "ssh -i my_key_pair ec2-user@${aws_instance.node-2.public_ip}"
 }
 
-output "worker-node-1-ssh" {
-     value = "ssh -i my_key_pair ec2-user@${aws_instance.worker-node-1.public_ip}"
+
+output "node-3-ip" {
+    value = aws_instance.node-1.public_ip 
 }
 
-output "worker-node-2-ssh" {
-     value = "ssh -i my_key_pair ec2-user@${aws_instance.worker-node-2.public_ip}"
+output "node-3-ssh" {
+    value = "ssh -i my_key_pair ec2-user@${aws_instance.node-3.public_ip}"
 }
